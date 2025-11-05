90b5b74bc50c1f6bb0ad5c26de55a2c0be72ea96
VictorNS <VictorNS@gmail.com>
2025-11-05 14:32:55 +0200

    Skip saving empty rows at end of settings

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,31 +1,30 @@
-﻿using DynamicExpresso;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
+using DynamicExpresso;
 using VariableCalculator;
 
 namespace WpfCoreCalculator
 {
 	public partial class MainWindow : Window
 	{
-		readonly string settingsFile = Path.ChangeExtension(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName, "json");
-		readonly List<TextBox> variables;
-		readonly List<TextBox> expressions;
-		readonly List<TextBox> results;
-		readonly List<TextBox> comments;
+		readonly string _settingsFile = Path.ChangeExtension(Environment.ProcessPath, "json");
+		readonly JsonSerializerOptions _serializerOptions = new()
+		{
+			WriteIndented = true,
+			Encoder = System.Text.Encodings.Web.JavaScriptEncoder.Create(System.Text.Unicode.UnicodeRanges.All)
+		};
+		readonly List<TextBox> _variables = [];
+		readonly List<TextBox> _expressions = [];
+		readonly List<TextBox> _results = [];
+		readonly List<TextBox> _comments = [];
 
 		public MainWindow()
 		{
 			InitializeComponent();
-
-			variables = new List<TextBox>();
-			expressions = new List<TextBox>();
-			results = new List<TextBox>();
-			comments = new List<TextBox>();
 		}
 
 		private void Window_Closed(object sender, System.EventArgs e)
@@ -36,29 +35,36 @@ namespace WpfCoreCalculator
 				Left = Left,
 				Height = Height,
 				Width = Width,
-				Rows = new List<Settings.Row>()
+				Rows = []
 			};
 
-			for (var i = 0; i < expressions.Count; i++)
+			int lastNonEmptyRow = -1;
+
+			for (var i = 0; i < _expressions.Count; i++)
 			{
-				settings.Rows.Add(new Settings.Row { Variable = variables[i].Text.Trim(), Expression = expressions[i].Text.Trim(), Comment = comments[i].Text.Trim() });
+				var item = new Settings.Row { Variable = _variables[i].Text.Trim(), Expression = _expressions[i].Text.Trim(), Comment = _comments[i].Text.Trim() };
+
+				if (!string.IsNullOrWhiteSpace(item.Variable) || !string.IsNullOrWhiteSpace(item.Expression) || !string.IsNullOrWhiteSpace(item.Comment))
+					lastNonEmptyRow = i;
+
+				settings.Rows.Add(item);
+
 			}
 
-			var newFile = JsonSerializer.Serialize(settings, new JsonSerializerOptions
-			{
-				WriteIndented = true,
-				Encoder = System.Text.Encodings.Web.JavaScriptEncoder.Create(System.Text.Unicode.UnicodeRanges.All)
-			});
+			if (lastNonEmptyRow != -1 && settings.Rows.Count > lastNonEmptyRow + 2)
+				settings.Rows = settings.Rows.GetRange(0, lastNonEmptyRow + 2);
+
+			var newFile = JsonSerializer.Serialize(settings, _serializerOptions);
 
 			bool createNewFile;
-			if (File.Exists(settingsFile))
+			if (File.Exists(_settingsFile))
 			{
-				var oldFile = File.ReadAllText(settingsFile);
+				var oldFile = File.ReadAllText(_settingsFile);
 				createNewFile = oldFile != newFile;
 
 				if (createNewFile)
 				{
-					File.Move(settingsFile, settingsFile + ".bak", true);
+					File.Move(_settingsFile, _settingsFile + ".bak", true);
 				}
 			}
 			else
@@ -68,21 +74,18 @@ namespace WpfCoreCalculator
 
 			if (createNewFile)
 			{
-				File.WriteAllText(settingsFile, newFile);
+				File.WriteAllText(_settingsFile, newFile);
 			}
 		}
 
 		private void Window_Loaded(object sender, RoutedEventArgs e)
 		{
 			Settings settings;
-			if (File.Exists(settingsFile))
+			if (File.Exists(_settingsFile))
 			{
 				try
 				{
-					settings = JsonSerializer.Deserialize<Settings>(File.ReadAllText(settingsFile), new JsonSerializerOptions
-					{
-						Encoder = System.Text.Encodings.Web.JavaScriptEncoder.Create(System.Text.Unicode.UnicodeRanges.All)
-					});
+					settings = JsonSerializer.Deserialize<Settings>(File.ReadAllText(_settingsFile), _serializerOptions);
 					settings.Height = Math.Max(110, settings.Height);
 					settings.Width = Math.Max(400, settings.Width);
 
@@ -93,11 +96,10 @@ namespace WpfCoreCalculator
 				}
 				catch (Exception ex)
 				{
-					AddGridRow(expressions.Count, "", "", "");
+					AddGridRow(_expressions.Count, "", "", "");
 					errorsLabel.Content = ex.Message;
 					return;
 				}
-
 			}
 			else
 			{
@@ -107,11 +109,11 @@ namespace WpfCoreCalculator
 					Left = 16,
 					Height = 200,
 					Width = 600,
-					Rows = new List<Settings.Row>
-					{
-						new Settings.Row{ Variable="v0", Expression="123"},
-						new Settings.Row{ Expression="v0*2"}
-					}
+					Rows =
+					[
+						new Settings.Row{ Variable = "v0", Expression = "123"},
+						new Settings.Row{ Expression = "v0*2"}
+					]
 				};
 			}
 
@@ -122,7 +124,7 @@ namespace WpfCoreCalculator
 
 			foreach (var item in settings.Rows)
 			{
-				AddGridRow(expressions.Count, item.Variable, item.Expression, item.Comment);
+				AddGridRow(_expressions.Count, item.Variable, item.Expression, item.Comment);
 			}
 
 			TextBox_TextChanged(null, null);
@@ -133,9 +135,9 @@ namespace WpfCoreCalculator
 			var errors = "";
 			var interpreter = new Interpreter();
 
-			for (var i = 0; i < expressions.Count; i++)
+			for (var i = 0; i < _expressions.Count; i++)
 			{
-				var expression = expressions[i].Text.Trim();
+				var expression = _expressions[i].Text.Trim();
 
 				double? result = null;
 				if (!string.IsNullOrWhiteSpace(expression))
@@ -154,24 +156,24 @@ namespace WpfCoreCalculator
 				if (result.HasValue)
 				{
 					var res = Math.Round(result.Value, 4);
-					results[i].Text = res.ToString();
+					_results[i].Text = res.ToString();
 
-					if (!string.IsNullOrWhiteSpace(variables[i].Text))
+					if (!string.IsNullOrWhiteSpace(_variables[i].Text))
 					{
-						interpreter.SetVariable(variables[i].Text.Trim(), res);
+						interpreter.SetVariable(_variables[i].Text.Trim(), res);
 					}
 				}
 				else
 				{
-					results[i].Text = "";
+					_results[i].Text = "";
 				}
 			}
 
 			errorsLabel.Content = errors.Length == 0 ? "OK" : errors;
 
-			if (expressions.Count==0 || !string.IsNullOrWhiteSpace(expressions[expressions.Count - 1].Text))
+			if (_expressions.Count == 0 || !string.IsNullOrWhiteSpace(_expressions[^1].Text))
 			{
-				AddGridRow(expressions.Count, "", "", "");
+				AddGridRow(_expressions.Count, "", "", "");
 			}
 		}
 
@@ -188,7 +190,7 @@ namespace WpfCoreCalculator
 			Grid.SetRow(var, rowIndex);
 			Grid.SetColumn(var, 0);
 			expressionsGrid.Children.Add(var);
-			variables.Add(var);
+			_variables.Add(var);
 
 			var eq = new Label { Content = "=" };
 			Grid.SetRow(eq, rowIndex);
@@ -200,7 +202,7 @@ namespace WpfCoreCalculator
 			Grid.SetRow(exp, rowIndex);
 			Grid.SetColumn(exp, 2);
 			expressionsGrid.Children.Add(exp);
-			expressions.Add(exp);
+			_expressions.Add(exp);
 
 			var res = new TextBox
 			{
@@ -211,13 +213,13 @@ namespace WpfCoreCalculator
 			Grid.SetRow(res, rowIndex);
 			Grid.SetColumn(res, 3);
 			expressionsGrid.Children.Add(res);
-			results.Add(res);
+			_results.Add(res);
 
-			var com = new TextBox { Text = newComment }; ;
+			var com = new TextBox { Text = newComment };
 			Grid.SetRow(com, rowIndex);
 			Grid.SetColumn(com, 4);
 			expressionsGrid.Children.Add(com);
-			comments.Add(com);
+			_comments.Add(com);
 		}
 	}
 }
